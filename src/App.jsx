46ca5cb5fa886ca9131c09d2f20ba8726656e1f3
ectrodev <ectroshop9@gmail.com>
2026-08-27 19:31:34 +0000

    fix: temporarily disable chatbot to save resources

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import AdminLiveChatPage from './pages/AdminLiveChatPage';
 import MobileBottomNav from './components/MobileBottomNav';
 
 // استيراد البوت
-import ChatBot from './components/ChatBot';
+// import ChatBot from './components/ChatBot';
 
 // مكون فرعي لإظهار الشريط السفلي فقط في صفحات الأدمن (باستثناء صفحة التسجيل)
 function LayoutWrapper({ children }) {
@@ -35,7 +35,7 @@ function LayoutWrapper({ children }) {
     <>
       {children}
       {isAdminRoute && <MobileBottomNav />}
-      <ChatBot />
+      {/* <ChatBot /> */}
     </>
   );
 }
