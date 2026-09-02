e8491640c26ea48949e61627471415f952625151
ectrodev <ectroshop9@gmail.com>
2026-09-02 10:09:58 +0000

    fix: vercel config for static + api

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
-    proxy: {
-      '/api': 'https://dzboard.onrender.com'
-    }
+    port: 5173,
   }
-})
+});
