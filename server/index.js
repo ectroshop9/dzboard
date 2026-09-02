730f6874a1b255cdb0d9485ecd36543b00b50aef
ectrodev <ectroshop9@gmail.com>
2026-09-02 09:57:18 +0000

    feat: convert to Vercel serverless

--- server/index.js
+++ server/index.js
@@ -49,6 +49,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
