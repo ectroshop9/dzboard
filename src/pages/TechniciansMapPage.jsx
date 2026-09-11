0cc1f0573b997c8d4810689150d74ef11ee9815f
ectrodev <ectroshop9@gmail.com>
2026-09-11 19:32:27 +0000

    fix: focus map on Algeria only with dark theme

--- src/pages/TechniciansMapPage.jsx
+++ src/pages/TechniciansMapPage.jsx
@@ -182,12 +182,16 @@ export default function TechniciansMapPage() {
             <MapContainer
               center={[28.0339, 1.6596]}
               zoom={5}
-              style={{ height: '100%', width: '100%' }}
+              minZoom={5}
+              maxZoom={10}
+              maxBounds={[[18.0, -9.0], [38.0, 12.5]]}
+              maxBoundsViscosity={1.0}
+              style={{ height: '100%', width: '100%', background: '#0a0a1a' }}
               scrollWheelZoom={true}
+              attributionControl={false}
             >
               <TileLayer
-                attribution='&copy; OpenStreetMap'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
               />
               {geoData && (
                 <GeoJSON
